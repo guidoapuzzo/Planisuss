7aca100bb1701581379320f4628c285121951469
Guido Apuzzo <98398020+guidoapuzzo@users.noreply.github.com>
2024-09-27 17:35:43 +0200

    Add files via upload

---
+++ constants.py
@@ -0,0 +1,19 @@
+import sys
+
+sys.path.append(".")
+
+### Game constants
+# Length of the simulation in days
+MAX_DAYS = 10000
+# Size of the (square) grid (NUMCELLS x NUMCELLS)
+NUM_CELLS = 50
+# Max life span of Carviz
+MAX_LIFE_C = 10
+# Max life span of Erbast
+MAX_LIFE_E = 10
+
+# Colours for terminal
+GREEN = '\033[92m'
+YELLOW = '\033[93m'
+RED = '\033[91m'
+RESET = '\033[1m'
---
+++ main.py
@@ -0,0 +1,6 @@
+from matplotlib import pyplot as plt
+from View.Simulator import SimulationView
+
+if __name__ == '__main__':
+    sim = SimulationView()
+    plt.show()
